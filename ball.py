a5f0e91b8543e0eeafcee156072826a2de9cc9b5
Joshua Hecker <jhecker2001@gmail.com>
2023-10-30 09:48:26 -0500

    Added functionality to bounce off of top and north walls, and edited the function for ball movement.

--- ball.py
+++ ball.py
@@ -1,10 +1,18 @@
 from turtle import Turtle
 
+TOP_HEIGHT = 280
+BOTTOM_HEIGHT = -280
+RIGHT_WIDTH = 380
+LEFT_WIDTH = -380
+MOVE = 10
+
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.create_ball()
+        self.x_move = MOVE
+        self.y_move = MOVE
 
     def create_ball(self):
         self.shape("circle")
@@ -13,5 +21,12 @@ class Ball(Turtle):
         self.goto(x=0, y=0)
 
     def move(self):
-        if self.xcor() < 380 and self.ycor() < 280:
-            self.goto(x=self.xcor() + 10, y=self.ycor() + 10)
+        if self.ycor() > TOP_HEIGHT or self.ycor() < BOTTOM_HEIGHT:
+            self.bounce()
+        new_y = self.ycor() + self.y_move
+        new_x = self.xcor() + self.x_move
+        if RIGHT_WIDTH > self.xcor() > LEFT_WIDTH:
+            self.goto(x=new_x, y=new_y)
+
+    def bounce(self):
+        self.y_move *= -1
